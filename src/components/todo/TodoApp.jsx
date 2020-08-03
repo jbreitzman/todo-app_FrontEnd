07680a6de89d0dae51431aa8541c69ff61d02cf4
jbreitzman <jbreitzman@gmail.com>
2020-08-03 11:52:32 -0400

    Removed functional login Components and replaced with JSX Expressions

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -25,8 +25,8 @@ class LoginComponent extends Component {
     render() {
         return (
             <div>
-                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
-                <ShowLoginSuccessful showSuccessMessage={this.state.showSuccessMessage} />
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+                {this.state.showSuccessMessage && <div>Login Successful</div>}
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
                 <button onClick={this.loginClicked}>Login</button>
@@ -48,20 +48,6 @@ class LoginComponent extends Component {
             [event.target.name]: event.target.value,
         });
     }
-}
-
-function ShowInvalidCredentials(props) {
-    if (props.hasLoginFailed) {
-        return <div>Invalid Credentials</div>;
-    }
-    return null;
-}
-
-function ShowLoginSuccessful(props) {
-    if (props.showSuccessMessage) {
-        return <div>Login Successful</div>;
-    }
-    return null;
 }
 
 export default TodoApp;
