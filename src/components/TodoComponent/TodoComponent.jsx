0fd6fc4ab4d38412350ed542a7264d20ca475bec
jbreitzman <jbreitzman@gmail.com>
2020-08-07 09:45:49 -0400

    Created TodoComponent, Added in 2 new Dependancies (Formik and Moment)

--- src/components/TodoComponent/TodoComponent.jsx
+++ src/components/TodoComponent/TodoComponent.jsx
@@ -1,17 +1,49 @@
 import React, { Component } from "react";
+import moment from "moment";
+import { Formik, Form, Field } from "formik";
 
 class TodoComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: 1,
+            id: this.props.match.params.id,
             description: "Learn Forms",
-            targetDate: new Date(),
+            targetDate: moment(new Date()).format("YYYY-MM-DD"),
         };
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
-        return <div>Todo Component for Id - {this.props.match.params.id}</div>;
+        let description = this.state.description;
+        let targetDate = this.state.targetDate;
+        return (
+            <div>
+                <h1>Todo</h1>
+                <div className="container">
+                    <Formik initialValues={{ description, targetDate }} onSubmit={this.onSubmit}>
+                        {(props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field className="form-control" type="text" name="description" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field className="form-control" type="date" name="targetDate" />
+                                </fieldset>
+                                <button className="btn btn-success" type="submit">
+                                    Save
+                                </button>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+                <div>Todo Component for Id - {this.props.match.params.id}</div>
+            </div>
+        );
     }
 }
 
